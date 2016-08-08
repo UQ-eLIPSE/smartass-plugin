48c6f732d07ffcb9d46066def31d7f395f33aaa5
Roy Portas <uqrport1@eait.uq.edu.au>
2016-08-08 06:44:54 +0100

    Got most of code working

--- src/test/java/au/edu/uq/smartass/question/AreaOfTriangleModuleTest.java
+++ src/test/java/au/edu/uq/smartass/question/AreaOfTriangleModuleTest.java
@@ -54,6 +54,25 @@ public class AreaOfTriangleModuleTest {
     }
 
     @Test
+    public void testAreaCalculation() throws Exception {
+        AreaOfTriangleModule triModule = new AreaOfTriangleModule();
+
+        AreaOfTriangleModule.TriangleVertex vert1 = triModule.new TriangleVertex("A", 1, 1, 1);
+        AreaOfTriangleModule.TriangleVertex vert2 = triModule.new TriangleVertex("B", 5, 1, 2);
+        AreaOfTriangleModule.TriangleVertex vert3 = triModule.new TriangleVertex("C", 1, 2, 3);
+
+        AreaOfTriangleModule.TriangleVertex vert12 = vert2.subtract(vert1);
+        AreaOfTriangleModule.TriangleVertex vert13 = vert3.subtract(vert1);
+
+        AreaOfTriangleModule.AreaCalculation calc = triModule.new AreaCalculation(vert12, vert13);
+
+        String expected = "$\\frac{\\sqrt{81.0}}{2} units^2$";
+
+        assertEquals(calc.getAnswer(), expected);
+
+    }
+
+    @Test
     public void testSubtract() throws Exception {
         AreaOfTriangleModule triModule = new AreaOfTriangleModule();
         AreaOfTriangleModule.TriangleVertex vert1 = triModule.new TriangleVertex("A", 1, 1, 1);
@@ -63,15 +82,15 @@ public class AreaOfTriangleModuleTest {
         assertEquals(result.getX(), 4);
         assertEquals(result.getY(), 0);
         assertEquals(result.getZ(), 1);
-        assertEquals(result.getName(), "AB");
-        assertEquals(result.formatString(), "AB(4, 0, 1)");
+        assertEquals(result.getName(), "\\vec{AB}");
+        assertEquals(result.formatString(), "\\vec{AB}(4, 0, 1)");
     }
 
     @Ignore
     @Test
     public void testGetSectionPackage() throws Exception {
         AreaOfTriangleModule dot = new AreaOfTriangleModule(new AreaOfTriangleModule.IntegerGenerator() {
-            private int[] ints = {3, 9, -9, 3, 2, 1, 4};
+            private int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9};
             private int idx = 0;
             @Override public int next(int lower, int uppper) {
                    return ints[idx++];
@@ -80,16 +99,25 @@ public class AreaOfTriangleModuleTest {
 
     }
 
-    @Ignore
     @Test
     public void testGetSection() throws Exception {
         AreaOfTriangleModule aot = new AreaOfTriangleModule(new AreaOfTriangleModule.IntegerGenerator() {
-            private int[] ints = {3, 3, -4, 4, -2, 1, 7};
+            private int[] ints = {1, 1, 1, 4, 5, 6, 2, 1, 3};
             private int idx = 0;
             @Override public int next(int lower, int uppper) {
                 return ints[idx++];
             }
         });
+
+        String expected = "Let A(1, 1, 1), B(4, 5, 6) and C(2, 1, 3) be the three vertices of a triangle. Determine the area of the triangle ABC.";
+        String actual = aot.getSection("question");
+
+        String actualAnswer = aot.getSection("answer");
+        String expectedAnswer = "$\\frac{\\sqrt{81.0}}{2} units^2$";
+        assertEquals(actualAnswer, expectedAnswer);
+
+        assertEquals(expected, actual);
+        
 
     }
 
