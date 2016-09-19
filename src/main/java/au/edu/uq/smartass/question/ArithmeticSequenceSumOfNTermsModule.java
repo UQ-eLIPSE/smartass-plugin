d4bc91e28ec2986eb72d9ecd764800cbdf5a0696
Andrew Mooney <a.mooney@uq.edu.au>
2016-09-19 12:20:25 +1000

    Added comments and refactored methods

--- src/main/java/au/edu/uq/smartass/question/ArithmeticSequenceSumOfNTermsModule.java
+++ src/main/java/au/edu/uq/smartass/question/ArithmeticSequenceSumOfNTermsModule.java
@@ -2,15 +2,14 @@ package au.edu.uq.smartass.question;
 
 import au.edu.uq.smartass.engine.QuestionModule;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.EnumMap;
 import java.util.Map;
 
 import static java.lang.String.format;
 
 /**
- * Created by uqamoon1 on 13/09/2016.
+ * Creates a randomised question, solution and answer
+ * Calculate the sum of the first n terms within a given sequence.
  */
 public class ArithmeticSequenceSumOfNTermsModule implements QuestionModule {
     /** Define supported TeX Sections. */
@@ -19,9 +18,14 @@ public class ArithmeticSequenceSumOfNTermsModule implements QuestionModule {
     /** Lookup TeX string. */
     private Map<Section,String> sectionTeX = new EnumMap<>(Section.class);
 
-    public int numA, numB, numC, diff, term, result;
+    int numA, numB, numC, diff, term, result;
 
-    /** Constructor used for testing only */
+    /**
+     * Constructor for ArithmeticSequenceSumOfNTermsModule takes 3 parameters
+     * @param numA First number in the sequence.
+     * @param diff Difference between each number in the sequence.
+     * @param term The nth term.
+     */
     ArithmeticSequenceSumOfNTermsModule(int numA, int diff, int term) {
         this.numA = numA;
         this.diff = diff;
@@ -30,10 +34,16 @@ public class ArithmeticSequenceSumOfNTermsModule implements QuestionModule {
         this.numC = this.numB + diff;
         this.result = term / 2 * (2 * numA + (term - 1) * diff);
 
-        createQuestionTex(this.numA, this.numB, this.numC, this.term);
-        createSolutionTex(this.numA, this.numB, this.diff, this.term, this.result);
+        sectionTeX.put(Section.QUESTION, createQuestionTex(this.numA, this.numB, this.numC, this.term));
+        sectionTeX.put(Section.SOLUTION, createSolutionTex(this.numA, this.numB, this.diff, this.term, this.result));
+        sectionTeX.put(Section.ANSWER, createAnswerTex(this.result));
     }
 
+    /**
+     * Publicly accessible constructor takes no parameters.
+     * Generates random values for numA, ratio and term.
+     * Passes random values to package private constructor.
+     */
     public ArithmeticSequenceSumOfNTermsModule() {
         this(
                 new QUtil().generatePosInt(2,2),
@@ -42,31 +52,42 @@ public class ArithmeticSequenceSumOfNTermsModule implements QuestionModule {
         );
     }
 
-    private void createQuestionTex(int numA, int numB, int numC, int term) {
-        String sb = format("Let $%1$d,%2$d,%3$d$ be an arithmetic sequence. Determine the sum of the first $%4$d$ terms in the sequence.\\\\", numA, numB, numC, term);
-        sectionTeX.put(Section.QUESTION, sb.toString());
-        try {
-            writeTexFile("question_output.tex", sb.toString());
-        } catch (Exception ex) {
-
-        }
+    /**
+     * Creates the LaTeX string for the Question Section.
+     * @param numA First number in the sequence.
+     * @param numB Second number in the sequence.
+     * @param numC Third number in the sequence.
+     * @param term The nth term.
+     * @return LaTeX formatted string for Question.
+     */
+    private String createQuestionTex(int numA, int numB, int numC, int term) {
+        return format("Let $%1$d,%2$d,%3$d$ be an arithmetic sequence. Determine the sum of the first $%4$d$ terms in the sequence.\\\\", numA, numB, numC, term);
     }
 
-    private void createSolutionTex(int numA, int numB, int diff, int term, int result) {
+    /**
+     * Creates the LaTeX string for the Solution Section.
+     * @param numA First number int the sequence.
+     * @param numB Second number in the sequence.
+     * @param diff Difference between each number in the sequence.
+     * @param term The nth term.
+     * @param result The final result of the sum of all numbers in sequence up to the nth term.
+     * @return LaTeX formatted string for Solution.
+     */
+    private String createSolutionTex(int numA, int numB, int diff, int term, int result) {
         String sb = format("$S_n=\\dfrac{n}{2}(2a+(n-1)d)$, where $d=%2$d-%1$d=%3$d$ and $a=%1$d$.\\\\", numA, numB, diff );
                 sb += format("Therefore $S_{%2$d}=\\dfrac{%2$d}{2}(2\\cdot %1$d +(%2$d-1)\\cdot %3$d)$\\\\", numA, term, diff );
                 sb += format("$=%2$d(%1$d+%3$d \\cdot%4$d)$\\\\", 2 * numA, term / 2, term - 1, diff);
                 sb += format("$=%d$", result);
-        sectionTeX.put(Section.SOLUTION, sb.toString());
-        try {
-            writeTexFile("solution_output.tex", sb.toString());
-        } catch (Exception ex) {
-
-        }
+        return sb;
     }
 
-    private void writeTexFile(String filename, String str) throws Exception {
-        Files.write(Paths.get("./" + filename), str.getBytes());
+    /**
+     * Creates the LaTeX string for the Answer Section.
+     * @param result The final result of the sum of all numbers in sequence up to the nth term
+     * @return LaTeX formatted string for Answer.
+     */
+    private String createAnswerTex(int result) {
+        return format("$=%d$", result);
     }
 
     /**
--- src/test/java/au/edu/uq/smartass/question/ArithmeticSequenceSumOfNTermsModuleTest.java
+++ src/test/java/au/edu/uq/smartass/question/ArithmeticSequenceSumOfNTermsModuleTest.java
@@ -1,20 +1,31 @@
 package au.edu.uq.smartass.question;
 
+import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.lang.reflect.Constructor;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
- * Created by uqamoon1 on 13/09/2016.
+ * Tests for ArithmeticSequenceSumOfNTermsModule.
  */
 public class ArithmeticSequenceSumOfNTermsModuleTest {
 
+    /**
+     * Create a global instance of ArithmeticSequenceSumOfNTermsModule using known values,
+     * provides a known output to test against.
+     */
     ArithmeticSequenceSumOfNTermsModule a = new ArithmeticSequenceSumOfNTermsModule(2, 4, 40);
+
+    /**
+     * Create a global instance of ArithmeticSequenceSumOfNTermsModule using random values.
+     */
     ArithmeticSequenceSumOfNTermsModule b = new ArithmeticSequenceSumOfNTermsModule();
 
     @Before
@@ -27,7 +38,10 @@ public class ArithmeticSequenceSumOfNTermsModuleTest {
 
     }
 
-
+    /**
+     * Test to ensure a constructor exists and is accessible.
+     * @throws Exception
+     */
     @Test
     public void testArithmeticSequenceSumOfNTermsModule() throws Exception {
         try {
@@ -39,6 +53,9 @@ public class ArithmeticSequenceSumOfNTermsModuleTest {
         }
     }
 
+    /**
+     * Test the generated properties against known values.
+     */
     @Test
     public void testGeneratedProperties() {
         assertEquals(10, a.numC);
@@ -46,6 +63,9 @@ public class ArithmeticSequenceSumOfNTermsModuleTest {
         assertEquals(3200, a.result);
     }
 
+    /**
+     * Test to compare the expected "Question" LaTeX output against a known string using known values.
+     */
     @Test
     public void testGetSectionQuestion() {
         String expected = "Let $2,6,10$ be an arithmetic sequence. Determine the sum of the first $40$ terms in the sequence.\\\\";
@@ -53,6 +73,9 @@ public class ArithmeticSequenceSumOfNTermsModuleTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test to compare the expected "Solution" LaTeX output against a known string using known values.
+     */
     @Test
     public void testGetSectionSolution() {
         String expected = "$S_n=\\dfrac{n}{2}(2a+(n-1)d)$, where $d=6-2=4$ and $a=2$.\\\\Therefore $S_{40}=\\dfrac{40}{2}(2\\cdot 2 +(40-1)\\cdot 4)$\\\\$=20(4+39 \\cdot4)$\\\\$=3200$";
@@ -60,21 +83,75 @@ public class ArithmeticSequenceSumOfNTermsModuleTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test to compare the expected "Answer" LaTeX output against a known string using known values.
+     */
     @Test
-    public void testRandomIntsQuestion() {
-        int numA =  b.numA, numB = b.numB, numC = b.numC, term = b.term, diff = b.diff;
-        ArithmeticSequenceSumOfNTermsModule c = new ArithmeticSequenceSumOfNTermsModule(numA, diff, term);
-        String expected = "Let $" + numA + "," + numB + "," + numC + "$ be an arithmetic sequence. Determine the sum of the first $" + term + "$ terms in the sequence.\\\\";
-        String actual = c.getSection("question");
+    public void testGetSectionAnswer() {
+        String expected = "$=3200$";
+        String actual = a.getSection("answer");
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test to compare the expected "Question" LaTeX output against an expected string using random values.
+     */
+    @Test
+    public void testRandomIntsQuestion() {
+        int numA =  b.numA,
+            diff = b.diff,
+            term = b.term;
+
+        ArithmeticSequenceSumOfNTermsModule c = new ArithmeticSequenceSumOfNTermsModule(numA, diff, term);
+        assertEquals(b.getSection("question"), c.getSection("question"));
+
+        try {
+            writeTexFile("question_output.tex", c.getSection("question"));
+        } catch (Exception ex) {
+            TestCase.fail();
+        }
+    }
+
+    /**
+     * Test to compare the expected "Solution" LaTeX output against an expected string using random values.
+     */
     @Test
     public void testRandomIntsSolution() {
-        int numA =  b.numA, numB = b.numB, term = b.term, diff = b.diff, result = b.result;
+        int numA =  b.numA,
+            diff = b.diff,
+            term = b.term;
+
         ArithmeticSequenceSumOfNTermsModule c = new ArithmeticSequenceSumOfNTermsModule(numA, diff, term);
-        String expected = "$S_n=\\dfrac{n}{2}(2a+(n-1)d)$, where $d=" + numB + "-" + numA + "=" + diff + "$ and $a=" + numA + "$.\\\\Therefore $S_{" + term + "}=\\dfrac{" + term + "}{2}(2\\cdot " + numA + " +(" + term + "-1)\\cdot " + diff + ")$\\\\$=" + (term / 2) + "(" + (2 * numA) + "+" + (term - 1) + " \\cdot" + diff + ")$\\\\$=" + result + "$";
-        String actual = c.getSection("solution");
-        assertEquals(expected, actual);
+        assertEquals(b.getSection("solution"), c.getSection("solution"));
+
+        try {
+            writeTexFile("solution_output.tex", c.getSection("solution"));
+        } catch (Exception ex) {
+
+        }
+    }
+
+    /**
+     * Test to compare the expected "Answer" LaTeX output against an expected string using random values.
+     */
+    @Test
+    public void testRandomIntsAnswer() {
+        int numA = b.numA,
+            diff = b.diff,
+            term = b.term;
+
+        ArithmeticSequenceSumOfNTermsModule c = new ArithmeticSequenceSumOfNTermsModule(numA, diff, term);
+        assertEquals(b.getSection("answer"), c.getSection("answer"));
+
+        try {
+            writeTexFile("answer_output.tex", c.getSection("answer"));
+        } catch (Exception ex) {
+
+        }
+
+    }
+
+    private void writeTexFile(String filename, String str) throws Exception {
+        Files.write(Paths.get("./" + filename), str.getBytes());
     }
 }
