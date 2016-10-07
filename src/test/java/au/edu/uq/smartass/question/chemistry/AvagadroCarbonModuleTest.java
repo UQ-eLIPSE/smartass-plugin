7c023848ee5a00a920bcdb77cb47666611046a06
Philip Waller <p.waller@uq.edu.au>
2016-10-07 15:55:28 +1000

    Implemented Avagadro with Carbon question.
    * Refactored abstract base class for Avagadro Question Modules.

---
+++ src/test/java/au/edu/uq/smartass/question/chemistry/AvagadroCarbonModuleTest.java
@@ -0,0 +1,68 @@
+package au.edu.uq.smartass.question.chemistry;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
+import static org.junit.Assert.*;
+
+/**
+ */
+public class AvagadroCarbonModuleTest {
+
+
+    @Test
+    public void AvagadroCarbonModuleDefault() {
+        try {
+            AvagadroCarbonModule.class.getConstructor();
+            assertTrue(true);
+        } catch (NoSuchMethodException ex) {
+            fail();
+        }
+    }
+
+    @Test
+    public void AvagadroCarbonModuleParam() {
+        avagadroCarbonModuleParamHelper(0.09);
+        avagadroCarbonModuleParamHelper(50.01);
+    }
+
+    private void avagadroCarbonModuleParamHelper(double carat) {
+        try {
+            new AvagadroCarbonModule(carat);
+            fail();
+        } catch (IllegalArgumentException ex) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void getSection() throws Exception {
+        AvagadroCarbonModule acm = new AvagadroCarbonModule(2.00);
+
+        String expected;
+
+        expected = "Calculate the number of carbon atoms that are in a $2.0$-carat diamond that weighs $0.4000$g.";
+        System.out.println(expected);
+        assertEquals(expected, acm.getSection("question"));
+
+        expected =
+				"\\newcommand{\\scientific}[1]" +
+                "{\\num[round-precision=4,round-mode=figures,scientific-notation=true]{#1}}\n" +
+				"The molar mass of carbon is $12.01$g mol$^{-1}$.\\\\\n" +
+				"The number of moles $=$ mass $\\div$ molar mass $=\\dfrac{0.40}{12.0}=0.0333$ moles.\\\\\n" +
+				"Therefore the number of molecules $=$ moles $\\times$ Avogadro’s number \n" +
+				"$=0.0333\\times(\\scientific{6.0220e+23})\n" +
+				"=\\scientific{2.0057e+22}$ molecules.";
+        System.out.println(expected);
+        assertEquals(expected, acm.getSection("solution"));
+
+        expected =
+				"\\newcommand{\\scientific}[1]{\\num[round-precision=4,round-mode=figures,scientific-notation=true]{#1}}\n" +
+				"\\scientific{2.0057e+22}";
+        System.out.println(expected);
+        assertEquals(expected, acm.getSection("answer"));
+
+    }
+
+}
--- src/test/java/au/edu/uq/smartass/question/chemistry/AvagadroGlucoseModuleTest.java
+++ src/test/java/au/edu/uq/smartass/question/chemistry/AvagadroGlucoseModuleTest.java
@@ -47,25 +47,31 @@ public class AvagadroGlucoseModuleTest {
     public void getQuestionTest() {
         String expected =
                 "Calculate the number of molecules in 5.00g of glucose powder $(C_6H_{12}O_6)$.";
+        System.out.println(expected);
         assertEquals(expected, qsn.getSection("question"));
     }
 
     @Test
     public void getSolutionTest() {
         String expected =
-				"The molar mass of glucose is $180.2$g mol$^{-1}$.\\\\\n" +
+				"\\newcommand{\\scientific}[1]{\\num[round-precision=4,round-mode=figures,scientific-notation=true]{#1}}\n" +
+				"The molar mass of glucose is $180.16$g mol$^{-1}$.\\\\\n" +
 				"The number of moles $=$ mass $\\div$ molar mass $=\\dfrac{5.00}{180.2}=0.0278$ moles.\\\\\n" +
-				"Therefore the number of molecules $=$ moles $\\times$ Avogadro’s number " +
-                "$=0.0278\\times(" +
-                "\\num[round-precision=4,round-mode=figures,scientific-notation=true]{6.022000e+23}" +
-                ")=\\num[round-precision=4,round-mode=figures,scientific-notation=true]{1.671292e+22}$ molecules.";
+				"Therefore the number of molecules $=$ moles $\\times$ Avogadro’s number \n" +
+                "$=0.0278\\times(\\scientific{6.0220e+23})\n" +
+                "=\\scientific{1.6713e+22}$ molecules."
+           ;
         System.out.println(expected);
         assertEquals(expected, qsn.getSection("solution"));
     }
 
     @Test
     public void getAnswerTest() {
-        String expected = "\\num[round-precision=4,round-mode=figures,scientific-notation=true]{1.671292e+22}";
+        String expected =
+				"\\newcommand{\\scientific}[1]" +
+                "{\\num[round-precision=4,round-mode=figures,scientific-notation=true]{#1}}\n" +
+				"\\scientific{1.6713e+22}";
+        System.out.println(expected);
         assertEquals(expected, qsn.getSection("answer"));
     }
 
