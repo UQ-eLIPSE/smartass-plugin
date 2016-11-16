614676763e939a33f87c926cc972d95ea0e177bc
Philip Waller <p.waller@uq.edu.au>
2016-11-16 18:18:07 +1000

    Implemented 'Integration by Substitution' question.

---
+++ src/main/java/au/edu/uq/smartass/question/IntegrateBySubstitutionModule.java
@@ -0,0 +1,94 @@
+package au.edu.uq.smartass.question;
+
+import au.edu.uq.smartass.engine.SimpleQuestionModule;
+
+/**
+ */
+public class IntegrateBySubstitutionModule extends SimpleQuestionModule {
+
+
+    public IntegrateBySubstitutionModule() { }
+
+    IntegrateBySubstitutionModule(final int a, final int m, final int b, final int n, final int S) {
+        assert( 0 < a && a <= 10 );
+        assert( 0 < m && m <= 10 );
+        assert( 0 < b && b <= 10 );
+        assert( 0 <= n && n < m );
+        assert( 0 < S && S <= 10 );
+
+        String poly = formatPolynomial(a, m, b, n);
+        String derv = formatPolynomial(a*m, m-1, b*n, n-1);
+        String numr = formatPolynomial(S*a*m, m-1, S*b*n, n-1);
+        String ques = String.format("\\displaystyle\\int\\dfrac{%1$s}{%2$s}\\,\\mathrm{d}x", numr, poly);
+        String ans = String.format( "%2$s\\ln|%1$s|+C", poly, 1 == S ? "" : Integer.toString(S) );
+
+        setQuestion(createQuestionTex(ques));
+        setSolution(createSolutionTex(ques, poly, derv, ans, S));
+        setAnswer(createAnswerTex(ans));
+    }
+
+    private String createQuestionTex(final String question) {
+        String tex =
+                String.format("Determine $%1$s$.", question);
+        return tex;
+    }
+
+    private String createSolutionTex(
+            final String ques, final String poly, final String derv, final String ans,
+            final int S
+    ) {
+        String tex =
+                "\\begin{align*}\n" +
+                String.format("\\text{Let }u&=%s.\\\\\n", poly) +
+                String.format("\\text{Then }\\dfrac{\\mathrm{d}u}{\\mathrm{d}x}&=%s.\\\\\n", derv) +
+                String.format("\\text{So }\\mathrm{d}u&=%s\\,\\mathrm{d}x.\n", derv) +
+                "\\end{align*}\n" +
+                "\\begin{align*}\n" +
+                ques + "\n" +
+                String.format("&=\\displaystyle\\int\\dfrac{%d}{u}\\,\\mathrm{d}u\\\\\n", S) +
+                String.format("&=%s\\ln|u|+C\\\\\n", 1==S ? "" : Integer.toString(S)) +
+                String.format("&=%1$s\n", ans) +
+                "\\end{align*}";
+        return tex;
+    }
+
+    private String createAnswerTex(final String ans) {
+        String tex = String.format("$%1$s$", ans);
+        return tex;
+    }
+
+    private String formatPolynomial(final int a, final int m, final int b, final int n) {
+        String term_1 = formatPolyTerm(a, m);
+        String term_2 = formatPolyTerm(b, n);
+        String tex = String.format("%1$s%2$s%3$s",
+                term_1,
+                term_2.startsWith("-") || term_2.isEmpty() ? "" : "+",
+                term_2
+            );
+        return tex;
+    }
+
+    /**
+     *
+     * @param a
+     * @param m
+     * @return
+     *          empty string if a == 0
+     *          'a' if m == 0
+     *          '[sign]x^{m}' if 1 == |a|
+     *          'ax^{m}' generally
+     */
+    private String formatPolyTerm(final int a, final int m) {
+        String tex = "";
+        if ( 0 == a ) ;     // do nothing - empty Term
+        else if ( 0 == m ) tex += String.format("%d", a);
+        else {
+            String xpart = (1 == m) ? "x" : String.format("x^{%d}", m);
+            if ( 1 == Math.abs(a) )
+                tex += String.format("%s%s", a < 0 ? "-" : "", xpart);
+            else
+                tex += String.format("%d%s", a, xpart);
+        }
+        return tex;
+    }
+}
---
+++ src/test/java/au/edu/uq/smartass/question/IntegrateBySubstitutionModuleTest.java
@@ -0,0 +1,153 @@
+package au.edu.uq.smartass.question;
+
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
+import static org.junit.Assert.*;
+
+/**
+ */
+public class IntegrateBySubstitutionModuleTest {
+
+    /**
+     * Check for 'public' default constructor.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void IntegrateBySubstitutionModuleDefault() throws Exception {
+        try {
+            Constructor<IntegrateBySubstitutionModule> constructor
+                    = IntegrateBySubstitutionModule.class.getConstructor();
+            assertTrue(true);
+        } catch (NoSuchMethodException ex) {
+            fail();
+        }
+    }
+
+    @Test
+    public void IntegrateBySubstitutionModuleConstructor() throws Exception {
+        //constructionPass(1);
+        //constructionFail( 0);
+    }
+
+    private void constructionPass(int a) {
+        try {
+            //new IntegrateBySubstitutionModule(a);
+            assert(true);
+        } catch (Exception ex) {
+            fail();
+        }
+    }
+
+    private void constructionFail(int a) {
+        try {
+            //new IntegrateBySubstitutionModule(a);
+            fail();
+        } catch (AssertionError er) {
+            assert(true);
+        }
+    }
+
+    @Test
+    public void testQuestionLaTeX_sample() throws Exception {
+        IntegrateBySubstitutionModule integral = new IntegrateBySubstitutionModule(1, 2, 1, 1, 1);
+
+        String expectedQ =
+                "Determine $\\displaystyle\\int\\dfrac{2x+1}{x^{2}+x}\\,\\mathrm{d}x$.";
+        String actualQ = integral.getSection("Question");
+        System.out.println(actualQ);
+        assertEquals(expectedQ, actualQ);
+
+        String expectedS =
+                "\\begin{align*}\n" +
+                "\\text{Let }u&=x^{2}+x.\\\\\n" +
+                "\\text{Then }\\dfrac{\\mathrm{d}u}{\\mathrm{d}x}&=2x+1.\\\\\n" +
+                "\\text{So }\\mathrm{d}u&=2x+1\\,\\mathrm{d}x.\n" +
+                "\\end{align*}\n" +
+                "\\begin{align*}\n" +
+                "\\displaystyle\\int\\dfrac{2x+1}{x^{2}+x}\\,\\mathrm{d}x\n" +
+                "&=\\displaystyle\\int\\dfrac{1}{u}\\,\\mathrm{d}u\\\\\n" +
+                "&=\\ln|u|+C\\\\\n" +
+                "&=\\ln|x^{2}+x|+C\n" +
+                "\\end{align*}";
+        String actualS = integral.getSection("Solution");
+        System.out.println(actualS);
+        assertEquals(expectedS, actualS);
+
+        String expectedA =
+                "$\\ln|x^{2}+x|+C$";
+        String actualA = integral.getSection("Answer");
+        System.out.println(actualA);
+        assertEquals(expectedA, actualA);
+
+    }
+
+    @Test
+    public void testQuestionLaTeX_small() throws Exception {
+        IntegrateBySubstitutionModule integral = new IntegrateBySubstitutionModule(1, 1, 1, 0, 1);
+
+        String expectedQ =
+                "Determine $\\displaystyle\\int\\dfrac{1}{x+1}\\,\\mathrm{d}x$.";
+        String actualQ = integral.getSection("Question");
+        System.out.println(actualQ);
+        assertEquals(expectedQ, actualQ);
+
+        String expectedS =
+                "\\begin{align*}\n" +
+                "\\text{Let }u&=x+1.\\\\\n" +
+                "\\text{Then }\\dfrac{\\mathrm{d}u}{\\mathrm{d}x}&=1.\\\\\n" +
+                "\\text{So }\\mathrm{d}u&=1\\,\\mathrm{d}x.\n" +
+                "\\end{align*}\n" +
+                "\\begin{align*}\n" +
+                "\\displaystyle\\int\\dfrac{1}{x+1}\\,\\mathrm{d}x\n" +
+                "&=\\displaystyle\\int\\dfrac{1}{u}\\,\\mathrm{d}u\\\\\n" +
+                "&=\\ln|u|+C\\\\\n" +
+                "&=\\ln|x+1|+C\n" +
+                "\\end{align*}";
+        String actualS = integral.getSection("Solution");
+        System.out.println(actualS);
+        assertEquals(expectedS, actualS);
+
+        String expectedA =
+                "$\\ln|x+1|+C$";
+        String actualA = integral.getSection("Answer");
+        System.out.println(actualA);
+        assertEquals(expectedA, actualA);
+    }
+
+    @Test
+    public void testQuestionLaTeX_big() throws Exception {
+        IntegrateBySubstitutionModule integral =
+                new IntegrateBySubstitutionModule(10, 10, 10, 9, 10);
+
+        String expectedQ =
+                "Determine $\\displaystyle\\int\\dfrac{1000x^{9}+900x^{8}}{10x^{10}+10x^{9}}\\,\\mathrm{d}x$.";
+        String actualQ = integral.getSection("Question");
+        System.out.println(actualQ);
+        assertEquals(expectedQ, actualQ);
+
+        String expectedS =
+                "\\begin{align*}\n" +
+                "\\text{Let }u&=10x^{10}+10x^{9}.\\\\\n" +
+                "\\text{Then }\\dfrac{\\mathrm{d}u}{\\mathrm{d}x}&=100x^{9}+90x^{8}.\\\\\n" +
+                "\\text{So }\\mathrm{d}u&=100x^{9}+90x^{8}\\,\\mathrm{d}x.\n" +
+                "\\end{align*}\n" +
+                "\\begin{align*}\n" +
+                "\\displaystyle\\int\\dfrac{1000x^{9}+900x^{8}}{10x^{10}+10x^{9}}\\,\\mathrm{d}x\n" +
+                "&=\\displaystyle\\int\\dfrac{10}{u}\\,\\mathrm{d}u\\\\\n" +
+                "&=10\\ln|u|+C\\\\\n" +
+                "&=10\\ln|10x^{10}+10x^{9}|+C\n" +
+                "\\end{align*}";
+        String actualS = integral.getSection("Solution");
+        System.out.println(actualS);
+        assertEquals(expectedS, actualS);
+
+        String expectedA =
+                "$10\\ln|10x^{10}+10x^{9}|+C$";
+        String actualA = integral.getSection("Answer");
+        System.out.println(actualA);
+        assertEquals(expectedA, actualA);
+    }
+}
